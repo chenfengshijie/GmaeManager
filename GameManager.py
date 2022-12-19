4d10a68f738967d753b22538d31ac2c3fc2ee163
495964337 <495964337@qq.com>
2022-12-19 16:19:12 +0800

    add timer

--- GameManager.py
+++ GameManager.py
@@ -4,13 +4,14 @@ import json
 from SendMail import Email
 import datetime
 
+
 class GameManager:
-    def __init__(self,setting_files="setting.json"):
-        setting_files = open(setting_files,"r")
+    def __init__(self, setting_files="setting.json"):
+        setting_files = open(setting_files, "r")
         setting = json.load(setting_files)
         self.setting = setting
-        self.email = Email(setting["Setting"]["EmailAddress"],setting["Setting"]["EmailPassword"],
-        setting["Setting"]["EmailHost"])
+        self.email = Email(setting["Setting"]["EmailAddress"], setting["Setting"]["EmailPassword"],
+                           setting["Setting"]["EmailHost"])
         self.game = setting["Setting"]["Game"]
         self.time = setting['Setting']['Time']
         self.restricted_user = setting['Setting']['Time']
@@ -20,31 +21,15 @@ class GameManager:
         self.password = setting["Setting"]["Password"]
         setting_files.close()
 
-    def modify_setting(self,file_name):
-        """Modify the settings file"""
-        if not os.path.exists(file_name):
-            raise Exception("File not found: %s" % file_name)
-        if file_name.endswith(".json"):
-            raise Exception("input a json file")
-        
-        setting = json.load(file_name)
-        self.email = Email(setting["Setting"]["EmailAddress"],setting["Setting"]["EmailPassword"],
-        setting["Setting"]["EmialHost"])
-        self.game = setting["Setting"]["Game"]
-        self.time = setting['Setting']['Time']
-        self.restricted_user = setting['Setting']['Time']
-        # TODO : modify_setting
-        print("Setting modift successfully")
-
     # TODO:update time every 5 minutes
-    def __update_time(self):
-        """update time every 5 minutes
+    def update_time(self, flush_time):
+        """update time every flush time
         """
         for game in self.game:
-            cmd = "ps -ef | grep " + game # 该命令判断给定程序是否运行
+            cmd = "ps -ef | grep " + game + "| grep -V grep"  # 该命令判断给定程序是否运行
             f = os.popen(cmd)
             if f.read() != "":
-                self.used_time += 5 # 每隔五分钟更新
+                self.used_time += flush_time / 1000  # 每隔flush_time分钟更新
                 break
         if self.used_time > self.time:
             self.__kill_game()
@@ -52,9 +37,9 @@ class GameManager:
             info = "your child {} plays games for a long time, we close the game.".format(self.current_user)
             sender = self.email.email
             receiver = self.setting[self.current_user]["EmailAddress"]
-            self.email.send_email(info,sender,receiver)
-        
-        # 禁止在晚上10点之后玩游戏
+            self.email.send_email(info, sender, receiver)
+
+        # 禁止在晚上10点之后使用电脑
         now = datetime.datetime.now()
         flag = now.strftime("%H")
         if int(flag) > 22:
@@ -63,18 +48,15 @@ class GameManager:
             info = "your child {} plays games after 22:00".format(self.current_user)
             sender = self.email.email
             receiver = self.setting[self.current_user]["EmailAddress"]
-            self.email.send_email(info,sender,receiver)
-            
-            
-
+            self.email.send_email(info, sender, receiver)
 
     def __save_files(self):
         """save files
         """
         self.setting[self.current_user]["Time"] = self.used_time
         # 将游戏时间保存到文件中
-        with open("setting.json","w") as f:
-            json.dump(self.setting,fp=f)
+        with open("setting.json", "w") as f:
+            json.dump(self.setting, fp=f)
 
     def __kill_game(self):
         """kill game after exceed time limit
@@ -84,18 +66,15 @@ class GameManager:
             cmd = "killall " + game
             os.system(cmd)
 
-
     def main_loop(self):
-        self.modify_setting("setting.json")
+        """for debug"""
         while True:
             self.__update_time()
             self.__save_files()
             time.sleep(300)
 
-            
+
 if __name__ == "__main__":
     test = GameManager()
     print("run GameManager")
     GameManager.main_loop()
-
-
--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import json
 
 from GameManagerServer import TransFile
 from GameManager import GameManager
@@ -6,18 +7,21 @@ from GameManager_ui import Ui_MainWindow
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import QDateTime, QTimer
+
 
 class MyMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MyMainWindow, self).__init__()
         self.setupUi(self)
         self.game_manager = GameManager("setting.json")
+        self.lineEdit_4.setText(str(self.game_manager.used_time))
+        self.flush_time = 2000
 
-
+        self.timer_init()
         # 绑定信号
         self.pushButton.clicked.connect(self.verify_password)
-
+        self.pushButton_2.clicked.connect(self.modify_setting)
 
     def verify_password(self):
         password = self.lineEdit_3.text()
@@ -29,8 +33,28 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
             QMessageBox.warning(self.menubar, "警告", "密码错误", QMessageBox.Cancel, QMessageBox.Cancel)
             self.lineEdit.clear()
 
+    def modify_setting(self):
+        if self.lineEdit.focusPolicy() == QtCore.Qt.StrongFocus:
+            self.game_manager.setting["Setting"]["Time"] = int(self.lineEdit.text())
+            self.game_manager.setting["Setting"]["Game"] = self.lineEdit_2.text().split(",")
+            with open("setting.json", "w") as f:
+                json.dump(self.game_manager.setting, f)
 
-if __name__ =="__main__":
+            QMessageBox.information(self.menubar, "提示", "设置修改成功", QMessageBox.Cancel, QMessageBox.Cancel)
+        else:
+            QMessageBox.information(self.menubar, "提示", "请先输入密码", QMessageBox.Cancel, QMessageBox.Cancel)
+
+    def timer_init(self):
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.modify_time)
+        self.timer.start(self.flush_time)
+
+    def modify_time(self):
+        self.game_manager.update_time(self.flush_time)
+        self.lineEdit_4.setText(str(self.game_manager.used_time))
+
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MyMainWindow()
     window.show()
